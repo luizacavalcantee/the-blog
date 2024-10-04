748e0ffd71cf88b475915fc0385e9b8be7d8365b
luizacavalcantee <cavalcanteluiza13@gmail.com>
2024-10-04 10:48:40 -0300

    finalizando o content e ajeitando botão de pesquisa

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -7,9 +7,9 @@ import fourthPost from '../../assets/post-4.png';
 export default function Content() {
   return (
     <div className="bg-white py-16">
-      <div className="container mx-auto px-20 border border-red-700">
-        <div className="flex gap-20 border border-red-700">
-            <div className="post w-[569px] border border-blue-700">
+      <div className="container mx-auto px-20">
+        <div className="flex gap-20">
+            <div className="post w-[569px]">
             <div>
                 <Image
                 src={firstPost} 
@@ -19,15 +19,15 @@ export default function Content() {
                 className='rounded-2xl'
                 />
             
-            <p className='date'>Janeiro 04, 2022</p>
-            <h2 className='text-darkPurple my-4'>Começando no ReactJS em 2022</h2>
-            <p className='w-[569px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis esse nam fugiat illo at quasi maiores odio rem natus? Asperiores nulla molestiae commodi corporis? Totam, exercitationem vel! Harum, deleniti voluptatum.</p>
+            <p className='mt-1'>Janeiro 04, 2022</p>
+            <h2 className='text-darkPurple my-2'>Começando no ReactJS em 2022</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis esse nam fugiat illo at quasi maiores odio rem natus? Asperiores nulla molestiae commodi corporis? Totam, exercitationem vel! Harum, deleniti voluptatum.</p>
             </div>
             </div>
-            <div className='w-[472px] border border-purple-700'>
+            <div className='w-[472px]'>
             <div>
                 <p className='data'>Janeiro 04, 2022</p>
-                <h2 className='text-darkPurple'>Conheça as principais técnicas para conseguir uma vaga internacional em programação</h2>
+                <h2 className='text-darkPurple mb-2'>Conheça as principais técnicas para conseguir uma vaga internacional em programação</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed pariatur quidem, unde, sit aperiam natus nihil, quaerat sunt nostrum numquam a! Voluptas odio pariatur voluptatibus alias voluptates repellat nihil dolor.</p>
             </div>
 
@@ -35,57 +35,45 @@ export default function Content() {
 
             <div>
                 <p className='data'>Janeiro 04, 2022</p>
-                <h2 className='text-darkPurple'>Veja a evolução do Front-end na prática</h2>
+                <h2 className='text-darkPurple mb-2'>Veja a evolução do Front-end na prática</h2>
                 <p>lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed pariatur quidem, unde, sit aperiam natus nihil, quaerat sunt nostrum numquam a! Voluptas odio pariatur voluptatibus alias voluptates repellat nihil dolor.</p>
             </div>
             </div> 
         </div>
-        
-        <div className='container flex my-16 gap-[30px]'>
-          <div className="w-[370px]">
-            <div>
-              <Image
-                src={secondPost} 
-                alt="Post" 
-                width={370}
-                height={400}
-                className='rounded-2xl'
-              />
-            
-            <p className='date'>Janeiro 04, 2022</p>
-            <h2 className='text-darkPurple my-4'>Começando no ReactJS em 2022</h2>
+
+        <div className='flex mt-16 gap-8'>
+          <div>
+            <Image
+              src={secondPost} 
+              alt="Post 1" 
+              width={370}
+              height={400}
+            />
+            <p className='mt-2'>Janeiro 04, 2022</p>
+            <h2 className='text-darkPurple my-1'>10 dicas para conseguir a vaga desejada</h2>
             <p className=''>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis esse nam fugiat illo at quasi maiores odio rem natus? Asperiores nulla molestiae commodi corporis? Totam, exercitationem vel! Harum, deleniti voluptatum.</p>
-            </div>
           </div>
-          <div className="">
-            <div>
-              <Image
-                src={thirdPost} 
-                alt="Post" 
-                width={370}
-                height={400}
-                className='rounded-2xl'
-              />
-            
-            <p className='date'>Janeiro 04, 2022</p>
-            <h2 className='text-darkPurple my-4'>Começando no ReactJS em 2022</h2>
+          <div>
+            <Image
+              src={thirdPost} 
+              alt="Post 1" 
+              width={370}
+              height={400}
+            />
+            <p className='mt-2'>Janeiro 04, 2022</p>
+            <h2 className='text-darkPurple my-1'>Deixe seu código mais semântico com essas dicas</h2>
             <p className=''>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis esse nam fugiat illo at quasi maiores odio rem natus? Asperiores nulla molestiae commodi corporis? Totam, exercitationem vel! Harum, deleniti voluptatum.</p>
-            </div>
           </div>
-          <div className="">
-            <div>
-              <Image
-                src={fourthPost} 
-                alt="Post" 
-                width={370}
-                height={400}
-                className='rounded-2xl'
-              />
-            
-            <p className='date'>Janeiro 04, 2022</p>
-            <h2 className='text-darkPurple my-4'>Começando no ReactJS em 2022</h2>
+          <div>
+            <Image
+              src={fourthPost} 
+              alt="Post 1" 
+              width={370}
+              height={400}
+            />
+            <p className='mt-2'>Janeiro 04, 2022</p>
+            <h2 className='text-darkPurple my-1'>Use essas dicas nas suas aplicações mobile</h2>
             <p className=''>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis esse nam fugiat illo at quasi maiores odio rem natus? Asperiores nulla molestiae commodi corporis? Totam, exercitationem vel! Harum, deleniti voluptatum.</p>
-            </div>
           </div>
         </div>
       </div>
--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import Logo from '../../assets/logo.svg';
-import Buscar from '../../assets/buscar.svg';
 import Arrow from '../../assets/arrow-right.svg';
 import Foto from '../../assets/featured-image.png';
+import Search from '../../assets/search.svg';
 
 export default function Home() {
   return (
     <div className="bg-darkPurple py-12">
-      <div className="container mx-auto px-20 border border-red-700">
+      <div className="container mx-auto px-20">
         <header className="flex items-center justify-between">
           <Image
             src={Logo}
@@ -17,28 +17,38 @@ export default function Home() {
             className='mr-28'
           />
           <div className="flex ml-auto gap-4 text-white mx-auto">
-            <div className="container mx-auto"> 
+            <div className="mx-auto flex items-center justify-center"> 
               <span className="border-l-2 border-green h-6 mx-[3px]"/>
-              <strong>Home</strong>
+              <h3>Home</h3>
             </div>
-            <div className="container mx-auto">Sobre</div>
-            <div className="container mx-auto">Categorias</div>
-            <div className="container mx-auto">Contato</div>
+            <div className='flex items-center justify-center'>
+              <h3 className="mx-auto">Sobre</h3>
+            </div>
+            <div className='flex items-center justify-center'>
+              <h3 className="mx-auto">Categorias</h3>
+            </div>
+            <div className='flex items-center justify-center'>
+              <h3 className="mx-auto">Contato</h3>
+            </div>
           </div>
-          <div>
-          <Image
-            src={Buscar}
-            alt="Logo"
-            width={300}
-            height={300}
-          />
-        </div>
-          
+
+          <div className='flex'>
+            <input type="text" placeholder='Buscar' className='bg-[#170027] text-white border-none outline-none ring-0 p-2 rounded-l-[5px] h-11' required />
+            <button className='bg-lightPurple h-[44] w-[60px] flex justify-center items-center rounded-r-[5px]'>
+              <Image
+                src={Search}
+                alt="lupa"
+                width={24}
+                height={24}
+              />
+            </button>
+          </div>
         </header>
+
         <div className='flex my-12 gap-6'>          
           <div className='flex-col items-center'>
             <h1 className='text-lightPurple'>Veja o guia definitivo para conquistar seus objetivos como DEV em 2022</h1>
-            <p className='text-light mt-4 mb-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos animi soluta reiciendis aspernatur tempore, ex quam! Adipisci delectus quasi aperiam nisi repudiandae natus, porro, esse tempora recusandae, autem voluptatem distinctio.</p>
+            <p className='text-light mt-4 mb-6 p-header'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos animi soluta reiciendis aspernatur tempore, ex quam! Adipisci delectus quasi aperiam nisi repudiandae natus, porro, esse tempora recusandae, autem voluptatem distinctio.</p>
             <div className='flex gap-2'>
               <span className='text-lightPurple'>Veja mais</span>
               <Image
